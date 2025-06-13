d45650e30d9423bc0cc163a39066a90a484bb3a2
kennys <>
2025-06-13 18:59:55 +0800

    修复 ios18 打不开网页的bug

--- cocos/platform/ios/CCApplication-ios.mm
+++ cocos/platform/ios/CCApplication-ios.mm
@@ -463,7 +463,8 @@ bool Application::openURL(const std::string &url)
 {
     NSString* msg = [NSString stringWithCString:url.c_str() encoding:NSUTF8StringEncoding];
     NSURL* nsUrl = [NSURL URLWithString:msg];
-    return [[UIApplication sharedApplication] openURL:nsUrl];
+    [[UIApplication sharedApplication] openURL:nsUrl options:nil completionHandler:nil];
+    return true;
 }
 
 void Application::copyTextToClipboard(const std::string &text)
